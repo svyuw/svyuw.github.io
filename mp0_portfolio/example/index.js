16aed6d02ddb9d33f7c7623d00ef5a94b43b7342
svyuw <76976595+svyuw@users.noreply.github.com>
2023-01-17 11:27:26 -0800

    working on extension reflection

---
+++ mp0_portfolio/example/index.js
@@ -0,0 +1,8 @@
+console.log("Welcome to my portfolio page!");
+
+<button onclick="myFunction()">Toggle dark mode</button>
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
