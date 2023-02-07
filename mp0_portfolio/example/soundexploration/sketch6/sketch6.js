26afac105c494b514de24da7a5e2648bec5310dc
svyuw <76976595+svyuw@users.noreply.github.com>
2023-02-06 20:24:24 -0800

    more mp2

--- mp0_portfolio/example/soundexploration/sketch6/sketch6.js
+++ mp0_portfolio/example/soundexploration/sketch6/sketch6.js
@@ -6,6 +6,7 @@ function setup() {
   mic.start();
   fft = new p5.FFT();
   fft.setInput(mic);
+  colorMode(HSB);
 }
 
 function draw() {
@@ -14,9 +15,14 @@ function draw() {
   noStroke();
   for (let i = 0; i < spectrum.length; i++) {
     let angle = map(i, 0, spectrum.length, 0, 360);
-    let h = map(angle, 0, 360, 0, 255);
     let s = map(spectrum[i], 0, 255, 100, 200);
     let b = map(spectrum[i], 0, 255, 100, 200);
+    let h;
+    if (spectrum[i] < 150) {
+      h = map(spectrum[i], 0, 150, 200, 100);
+    } else {
+      h = map(spectrum[i], 150, 255, 50, 0);
+    }
     fill(h, s, b);
     let r = map(spectrum[i], 0, 255, 20, 200);
     let x = width / 2 + r * cos(angle);
